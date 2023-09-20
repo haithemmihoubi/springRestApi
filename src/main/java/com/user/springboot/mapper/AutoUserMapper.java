9ef7336c6077876e3baab8dadd537bc36c9f3252
haithemmihoubi <haithemmihoubi1234@gmail.com>
2023-09-20 16:14:55 +0100

    Initial commit

---
+++ src/main/java/com/user/springboot/mapper/AutoUserMapper.java
@@ -0,0 +1,16 @@
+package com.user.springboot.mapper;
+
+import com.user.springboot.dto.UserDto;
+import com.user.springboot.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AutoUserMapper {
+
+    AutoUserMapper MAPPER = Mappers.getMapper(AutoUserMapper.class);
+
+    UserDto mapToUserDto(User user);
+
+    User mapToUser(UserDto userDto);
+}
---
+++ src/main/java/com/user/springboot/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.user.springboot.repository;
+
+import com.user.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+
+}
